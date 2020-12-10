982d15360ddd320aa10786ba668dd96a2a01a6fd
jinder1s <msingh@edx.org>
2020-12-10 12:47:10 -0500

    Remove unused import

--- cookiecutter-xblock/hooks/post_gen_project.py
+++ cookiecutter-xblock/hooks/post_gen_project.py
@@ -5,8 +5,6 @@ Post-generation cookiecutter hook.
 """
 import os
 
-from edx_lint.cmd.write import write_main
-
 from utils_edx_cookiecutters.layered_cookiecutter import LayeredCookiecutter
 
 layered_cookiecutter = LayeredCookiecutter(os.getcwd())
